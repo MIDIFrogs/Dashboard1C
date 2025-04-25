49e317af43275d0a8d827c4049e17bae0c1fc0d7
Ivan Olyanishin <ivan.ol.1704@gmail.com>
2025-04-26 00:40:48 +0500

    Create StatisticsService.cs

---
+++ src/backend/DashboardBackend/DashboardBackend.Core/StatisticsService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DashboardBackend.Data.Access;
+using DashboardBackend.Data.Models;
+
+namespace DashboardBackend.Core
+{
+    public class StatisticsService(IReportRepository reportRepository, ICategoryRepository categoryRepository, IProductGroupRepository productsRepository)
+    {
+        public async Task<decimal> CalculateCrossRebateAsync(int year, int quarter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<decimal> CalculatePointsAsync(int year, int quarter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<decimal> CalculatePointsAsync(Category category, int year, int quarter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<decimal> CalculateCompletionProgressAsync(ProductGroup product, int year, int quarter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<int> CountCompletedAsync(Category category, int year, int quarter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<decimal> CalculateCompletionProgressAsync(Category category, int year, int quarter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
